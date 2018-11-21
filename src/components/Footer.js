808e10b8239a2f6b97ef72f6bd5c86346bd03d88
Yiming <yanyiming@Yiming-Yan-MBP-15.local>
2018-11-21 18:22:21 -0500

    good

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,14 @@ import React from 'react';
 
 const Footer = () => (
   <footer>
-    <div> <small> Star me! </small> </div>
+    {/* <div><small> Star me! </small></div> */}
     <div>
-      <a className="github-button" href="https://github.com/jeffersonRibeiro/react-shopping-cart" data-size="large" aria-label="Star jeffersonRibeiro/react-shopping-cart on GitHub">Star</a>
+      <a className="github-button" href="https://github.com/yanym/react-ecommerce" data-size="large" aria-label="Star!!!!">Star me!!!</a>
     </div>
-    <div> Click ↘ and check my LinkedIn </div>
-    <a href="https://www.linkedin.com/in/yiming-yan-a6640111a/" rel="noopener noreferrer" target="_blank" title="Linkedin Profile">Yiming</a>
+    {/* <div> Click ↘ and check my LinkedIn </div> */}
+    <div>
+      <a href="https://www.linkedin.com/in/yiming-yan-a6640111a/" rel="noopener noreferrer" target="_blank" title="Linkedin Profile">My LinkedIn: Yiming</a>
+    </div>
   </footer>
 );
 
