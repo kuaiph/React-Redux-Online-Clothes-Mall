ca5e418e1e13150f429f3fe31964f4b08764eba9
Yiming <yanyiming@Yiming-Yan-MBP-15.local>
2018-11-23 13:31:43 -0500

    comm

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ app.use(cors());
 const port = 8123;
 
 app.get('/api/products', (req, res) => {
+  // In Node.js, __dirname is always the directory in which the currently executing script resides (see this). So if you typed __dirname into /d1/d2/myscript.js, the value would be /d1/d2.
     res.sendFile(path.join(__dirname, 'data', 'products.json'));
 });
 
 app.listen(port, () => {
-  console.log(`[products] API listening on port ${port}.`);
+  console.log(`[products] API listening on port ${ port }.`);
 });
