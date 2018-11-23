29b851a0a0b010adfd9f950fd6d0b744dec4a67c
Yiming <yanyiming@Yiming-Yan-MBP-15.local>
2018-11-23 13:38:44 -0500

    style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ const Footer = () => (
   <footer>
     {/* <div><small> Star me! </small></div> */}
     <div>
-      <a className="github-button" href="https://github.com/yanym/react-ecommerce" data-size="large" aria-label="Star!!!!">Star me!!!</a>
+      <a className = "github-button" href = "https://github.com/yanym/react-ecommerce" data-size = "large" aria-label = "Star!!!!">Star me!!!</a>
     </div>
     <div>
-      <a href="https://www.linkedin.com/in/yiming-yan-a6640111a/" rel="noopener noreferrer" target="_blank" title="Linkedin Profile">Click LinkedIn: Yiming</a>
+      <a href = "https://www.linkedin.com/in/yiming-yan-a6640111a/" rel = "noopener noreferrer" target = "_blank" title = "Linkedin Profile">Click LinkedIn: Yiming Yan</a>
     </div>
   </footer>
 );
--- src/components/Thumb.js
+++ src/components/Thumb.js
@@ -4,8 +4,8 @@ import PropTypes from "prop-types";
 
 const Thumb = props => {
   return (
-    <div className={props.classes}>
-      <img src={props.src} alt={props.alt} title={props.title} />
+    <div className = { props.classes }>
+      <img src = { props.src } alt = { props.alt } title = { props.title }/>
     </div>
   );
 };
